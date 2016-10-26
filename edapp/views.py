617a112693a1bd6d1b85af78b414ddb8e03890e3
sid <siddver007@gmail.com>
2016-10-26 22:52:34 +0530

    Views++

--- edapp/views.py
+++ edapp/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponseBadRequest, HttpResponse
 from django.template import RequestContext
 from collections import Counter
 from forms import UploadFileForm
-import pprint as pp
 import time
 
 def getServerHitTime(request):
@@ -13,10 +12,25 @@ def csvProcessView(request):
 	if request.method == "POST":
 		form = UploadFileForm(request.POST, request.FILES)
 		if form.is_valid():
+			start_time = time.time()
 			filehandle = request.FILES['file']
 			csv_list = filehandle.get_array()
 			duplicates = [k for k,v in Counter(map(tuple,csv_list)).items() if v>1]
-			return HttpResponse('The number of duplicates = ' + str(len(duplicates)))
+
+			duplicate_number = len(duplicates)
+
+			duplicate_rows_string = ''			
+			for row in duplicates:
+				duplicate_rows_string += str(row).replace('u\'','').replace('\'','') + '<br><br>'
+
+			if duplicate_rows_string != '':	
+				return HttpResponse('Number of duplicate rows = ' + '<b>' + str(duplicate_number) + '</b><br>'+ '(Approx. time taken : ' +
+					str('{0:.1f}'.format(time.time() - start_time)) + ' sec)' + '<br><br>' + '<br><br><b>Duplicate Rows(Tuples)</b>:-' +
+					'<br><br>' + duplicate_rows_string)
+			else:
+				return HttpResponse('Number of duplicate rows = ' + '<b>' + str(duplicate_number) + '</b><br>'+ '(Approx. time taken : ' + 
+					str('{0:.1f}'.format(time.time() - start_time)) + ' sec)' + '<br><br>' + '<br><br><b>Duplicate Rows(Tuples)</b>:-'
+					'<br><br>No duplicate rows')	
 		else:
 			return HttpResponseBadRequest()
 	else:
